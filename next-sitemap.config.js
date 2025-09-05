074a4bd0e50b937ea49ecbb3bc9bd09443945c6e
m-sherif-lutfy <m.sherif.lutfy@gmail.com>
2025-09-05 21:32:06 +0300

    chore(seo): add next-sitemap config and generated files

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://your-demo.vercel.app", // temporary; we’ll replace after deploy
+  generateRobotsTxt: true,
+};
